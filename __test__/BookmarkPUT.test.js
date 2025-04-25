cd9a74a182ef55d1aaadcfc00548c99bb88eb196
Anzzo <exp43@outlook.com>
2025-04-25 13:47:06 +0200

    Created GET and PUT endpoints for Bookmark

---
+++ __test__/BookmarkPUT.test.js
@@ -0,0 +1,24 @@
+const app = require('../app');
+const request = require('supertest');
+
+it('Change a boookmark values', async () => {
+	const userInformation = await request(app).post('/users/login').send({
+		username: 'teacher007',
+		password: 'asecret',
+	});
+
+	let token = JSON.parse(userInformation.text).data.token;
+	const response = await request(app).put('/bookmarks/1').set('Authorization', `Bearer ${token}`).send({ Name: 'Updated name', URL: 'http://localhost:8080' });
+	if (response.statusCode == 200) {
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toHaveProperty('message', 'Bookmark changed successful');
+	}
+	if (response.statusCode == 404) {
+		expect(response.statusCode).toBe(404);
+		expect(response.body).toHaveProperty('error', 'Bookmark not found');
+	}
+	if (response.statusCode == 500) {
+		expect(response.statusCode).toBe(500);
+		expect(response.body).toHaveProperty('error');
+	}
+});
